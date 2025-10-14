98914ee443591e7d4d1d463e12a2dd10110de003
Muflih Abqori <muflihabqori@gmail.com>
2025-10-14 12:07:46 +0700

    working with basic attribute

--- JQuery/index.js
+++ JQuery/index.js
@@ -21,3 +21,9 @@ $("button").addClass("margin-50").click(function() {
         $("button").removeClass("button-clicked padding-50").html("CLick This");
     },1000)
 });
+
+// assign src of the image to img tag in html. this will override the src in html if there is any
+$("img").attr("src", "")
+
+// assign href inside javascript, not html. this will override the href in html if there is any
+$("a").attr("href", "https://www.instagram.com/abqoryy");
