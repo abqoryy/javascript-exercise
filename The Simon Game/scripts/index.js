f119379ceff2d49725a7ac8ac4325bf98c335f58
Muflih Abqori <muflihabqori@gmail.com>
2025-10-18 11:40:21 +0700

    Simon Game - First Step

---
+++ The Simon Game/scripts/index.js
@@ -0,0 +1,27 @@
+// assign sound to each button when clicked
+
+
+const greenSounds = new Audio("sounds/green.mp3");
+const redSounds = new Audio("sounds/red.mp3");
+const yellowSounds = new Audio("sounds/yellow.mp3");
+const blueSounds = new Audio("sounds/blue.mp3");
+
+$(".bg-green").on("click", function() {
+    greenSounds.currentTime = 0;
+    greenSounds.play();
+});
+
+$(".bg-red").on("click", function() {
+    redSounds.currentTime = 0;
+    redSounds.play();
+});
+
+$(".bg-yellow").on("click", function() {
+    yellowSounds.currentTime = 0;
+    yellowSounds.play();
+});
+
+$(".bg-blue").on("click", function() {
+    blueSounds.currentTime = 0;
+    blueSounds.play();
+});
