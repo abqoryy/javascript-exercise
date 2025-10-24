05e0a952ce1c6555ecedcab452eaaf8f1e550bfd
Muflih Abqori <muflihabqori@gmail.com>
2025-10-24 10:00:26 +0700

    revision from scratch - Phase 1

--- The Simon Game/scripts/index.js
+++ The Simon Game/scripts/index.js
@@ -1,79 +1,48 @@
-// assign sound to each button when clicked
+// put all the button colors into an array
+// make empty array for patterns
+const buttonColors = ["green", "red", "yellow", "blue"];
+let gamePattern = [];
+let userPattern = [];
 
+
+// adding sounds to variables
 const greenSounds = new Audio("sounds/green.mp3");
 const redSounds = new Audio("sounds/red.mp3");
 const yellowSounds = new Audio("sounds/yellow.mp3");
 const blueSounds = new Audio("sounds/blue.mp3");
 
-$(".bg-green").on("click", function() {
-    greenSounds.currentTime = 0;
-    greenSounds.play();
+// start the game by clicking h1
+$(".title h1").on("click", function() {
+    nextSequence();
 });
 
-$(".bg-red").on("click", function() {
-    redSounds.currentTime = 0;
-    redSounds.play();
-});
-
-$(".bg-yellow").on("click", function() {
-    yellowSounds.currentTime = 0;
-    yellowSounds.play();
-});
-
-$(".bg-blue").on("click", function() {
-    blueSounds.currentTime = 0;
-    blueSounds.play();
-});
-
-// make pattern
-
-let gamePattern = [];
-const buttonColors = ["green", "red", "yellow", "blue"];
-
-let userPattern = [];
-// need to group this so the code only runs when the game starts
-nextSequence();
-
-// button click to verify userPattern to gamePattern
+// function to make action when user click the buttons
 $(".btn").on("click", function() {
     let userClick = $(this).attr("id");
-    userPattern.push(userClick); 
+    userPattern.push(userClick);
+
+    playSound(userClick);
+    // debuggin the click
     console.log(userClick);
 
     let currentIndex = userPattern.length - 1;
+    // matching both index patterns
     if (userPattern[currentIndex] === gamePattern[currentIndex]) {
+        // debuggin if the patterns are matched
         console.log("success");
-    } else {
-        console.log("wrong");
-    }
 
-    if (userPattern.length === gamePattern.length) {
-        userPattern = [];
-        
-        setTimeout(() => {
-            nextSequence();
-        },1000);
+        // check if user already finished the sequence
+        if (userPattern.length === gamePattern.length) {
+            setTimeout(() => {
+                nextSequence();
+            },1000);
+        };
+    } else {
+        // debuggin if the patterns are not matched
+        // MAKE NEW CLASS FOR WRONG ANSWER IN CSS
+        console.log("wrong");
     };
+
 });
 
-
-function nextSequence() {
-    let randomNumber = Math.floor(Math.random() * 4);
-    let randomChosen = buttonColors[randomNumber];
-    gamePattern.push(randomChosen);
-
-    playSequence();
-
-};
-
-function playSequence() {
-    for (let i = 0; i < gamePattern.length && i < 5; i++) {
-        let color = gamePattern[i];
-        setTimeout(() => {       
-            $("#" + color).addClass("highlight");
-            setTimeout(() => {
-                $("#" + color).removeClass("highlight");
-            },500);
-        }, i * 1000);
-    }
-};
+// 
